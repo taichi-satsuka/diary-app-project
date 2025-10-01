b50a999a0fbeffb07518797819e1e56c6163ac98
Taichi Satsuka <taichisatsuka@PC22027.local>
2025-10-01 12:31:00 +0900

    feat: create nuxt project and install graphql-client

---
+++ frontend/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>Hello world</h1>
+</template>
+
+<script>
+import { request, gql } from 'graphql-request'
+
+const query = gql`
+  {
+    user(id: 2) {
+        id
+        name
+        email
+    }
+  }
+`
+
+request('http://localhost/graphql', query).then((data) => console.log(data))
+</script>
