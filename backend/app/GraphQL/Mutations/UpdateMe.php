66e2aa493b6961120dd995046797bd7c5719a53f
Taichi Satsuka <taichisatsuka@PC22027.local>
2025-10-10 18:23:22 +0900

    feat: upodateMe(update bio or profile_image_url)

---
+++ backend/app/GraphQL/Mutations/UpdateMe.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+
+namespace App\GraphQL\Mutations;
+
+use Illuminate\Support\facades\Auth;
+
+final readonly class UpdateMe
+{
+    /** @param  array{}  $args */
+    public function __invoke(null $_, array $args)
+    {
+        $input = $args['input'];
+        $user = Auth::user();
+        $updated = false;
+
+        if (isset($input['bio']) && $input['bio'] !== $user->bio) {
+            $user->bio = $input['bio'];
+            $updated = true;
+        }
+        
+        if (isset($input['profile_image_url']) && $input['profile_image_url'] !== $user->profile_image_url) {
+            $user->profile_image_url = $input['profile_image_url'];
+            $updated = true;
+        }
+
+        if ($updated) {
+            $user->save();
+
+            return [
+                'success' => true,
+                'message' => "Your profile was updated.",
+                'user' => $user,
+            ];
+        }
+
+        return [
+            'success' => false,
+            'message' => "Nothing was updated",
+            'user' => null,
+        ];
+    }
+}
