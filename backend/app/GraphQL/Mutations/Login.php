20f80a4ea9d7074f945bbd25597ca54d173e8d45
Taichi Satsuka <taichisatsuka@PC22027.local>
2025-10-10 12:44:24 +0900

    feat: login api

---
+++ backend/app/GraphQL/Mutations/Login.php
@@ -0,0 +1,48 @@
+<?php declare(strict_types=1);
+
+namespace App\GraphQL\Mutations;
+
+use App\Models\User;
+use App\GraphQL\Responses\AuthResponse;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+final readonly class Login
+{
+    /** @param  array{}  $args */
+    public function __invoke(null $_, array $args)
+    {
+        $input = $args['input'];
+        
+        $validator = Validator::make($input, [
+            'email' => 'required|email',
+            'password' => 'required|string|min:8',
+        ]);
+
+        if ($validator->fails()) {
+            return (new AuthResponse(
+                success: false,
+                message: $validator->errors()->first()
+            ))->toArray();
+        }
+
+        $user = User::where('email', $input['email'])->first();
+
+        if (!$user || !Hash::check($input['password'], $user->password)) {
+            return (new AuthResponse(
+                success: false,
+                message: "Invalid email or password."
+            ))->toArray();
+        }
+
+        // パーソナルアクセストークン発行
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return (new AuthResponse(
+            success: true,
+            message: "Login successful.",
+            user: $user,
+            token: $token
+        ))->toArray();
+    }
+}
