602555c6966f4b8ce71233b8d4ade910b914e92b
Taichi Satsuka <taichisatsuka@PC22027.local>
2025-10-23 14:06:55 +0900

    feat: login form

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -1,34 +1,34 @@
 <template>
-    <div class="min-h-screen flex justify-center items-center bg-gray-100">
-        <div class="rounded-lg bg-white w-full max-w-md p-4 shadow-xl">
-            <h2 class="text-2xl text-center text-gray-1000 my-2">Sign in to your account</h2>
-            <form action="">
-                <div class="mb-2">
-                    <label for="email" class="block">Your email</label>
+    <div class="min-h-screen flex justify-center items-center bg-gradient-to-br from-teal-100 to-teal-300">
+        <div class="rounded-2xl bg-white w-full max-w-md p-8 shadow-2xl">
+            <h2 class="text-2xl text-center text-gray-700 mb-6 font-bold">Sign in to your account</h2>
+            <form action="" class="space-y-5 ">
+                <div>
+                    <label for="email" class="block text-sm mb-1 ">Your email</label>
                     <input
                         type="email"
                         name="email"
                         id="email"
-                        class="px-4 py-2 bg-gray-100 border border-gray-300 rounded-md w-full shadow-sm"
+                        class="px-4 py-3 bg-gray-100 border border-gray-300 shadow-sm rounded-md w-full shadow-sm focus:outline-none focus:ring-2 focus:ring-teal-400 transition"
                         placeholder="your@example.com"
                         required
                     />
                 </div>
-                <div class="mb-2">
-                    <label for="password" class="block">password</label>
+                <div>
+                    <label for="password" class="block text-sm mb-1">password</label>
                     <input
                         type="password"
                         name="password"
                         id="password"
                         placeholder="password"
-                        class="px-4 py-2 bg-gray-100 border border-gray-300 rounded-md w-full shwdow-sm"
+                        class="px-4 py-3 bg-gray-100 border border-gray-300 rounded-md shadow-sm w-full focus:outline-none focus:ring-2 focus:ring-teal-400 transition"
                     />
                 </div>
-                <div class="mb-2">
-                    <button type="button" class="px-4 py-2 w-full text-center rounded-md bg-sky-500 text-white hover:bg-sky-600">Sign in</button>
+                <div>
+                    <button type="button" class="px-4 py-2 mt-2 w-full text-center rounded-md bg-teal-500 shadow-md text-white hover:bg-teal-600 hover:shadow-lg font-semibold transition">Sign in</button>
                 </div>
             </form>
-            <p class="text-center">Don't have an account yet?   <NuxtLink to="/signup" class="text-sky-500 hover:text-sky-600">Sign up</NuxtLink></p>
+            <p class="text-gray-500 text-sm text-center mt-6">Don't have an account yet?   <NuxtLink to="/signup" class="text-teal-500 hover:text-teal-600 hover:underline">Sign up</NuxtLink></p>
             
         </div>
     </div>
