60d8eac4cb5d0bc3a0acb3bc2e60ad039b1157a8
Taichi Satsuka <taichisatsuka@PC22027.local>
2025-10-30 12:04:30 +0900

    feat: create diary modal

---
+++ frontend/components/PostModal.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="min-h-screen fixed inset-0 flex justify-center items-center backdrop-blur-sm bg-gradient-to-br from-teal-100/70 to-teal-200/70 z-50">
+        <div class="w-full max-w-md bg-white rounded-2xl p-8">
+            <h2 class="text-center font-extrabold text-2xl mb-3">Crete Diary</h2>
+            <form action="" class="space-y-5">
+                <div>
+                    <label for="title" class="block text-md mb-1">title</label>
+                    <input
+                        type="text"
+                        name="title"
+                        id="title"
+                        class="px-4 py-3 bg-gray-100 border border-gray-300 shadow-sm rounded-md w-full shadow-sm focus:outline-none focus:ring-2 focus:ring-teal-400 transition"
+                        placeholder="Your title"
+                        required
+                    />
+                </div>
+                <div>
+                    <lable class="block text-md mb-1">content</lable>
+                    <textarea
+                        name="content"
+                        id="content"
+                        class="px-4 py-3 h-60 bg-gray-100 border border-gray-300 shadow-sm rounded-md w-full shadow-sm focus:outline-none focus:ring-2 focus:ring-teal-400 transition resize-none"
+                        placeholder="Your content"
+                    ></textarea>
+                </div>
+                <div class="flex flex-row-reverse gap-1">
+                    <div>
+                        <input 
+                            type="radio"
+                            name="visibility"
+                            id="followers"
+                            class="hidden peer"
+                            value="followers"
+                            required
+                        />
+                        <label 
+                            for="followers"
+                            class="text-center px-3 py-1 border-2 rounded-md hover:border-teal-400 transition peer-checked:bg-teal-300"
+                        >Only Followers</label>
+                    </div>
+                    <div>
+                        <input 
+                            type="radio"
+                            name="visibility"
+                            id="public"
+                            class="hidden peer"
+                            value="public"
+                            required
+                        />
+                        <label 
+                            for="public"
+                            class="text-center px-3 py-1 border-2 rounded-md hover:border-teal-400 transition peer-checked:bg-teal-300"
+                        >public</label>
+                    </div>
+                </div>
+            </form>
+            <div class="flex flex-row-reverse gap-2 mt-4">
+                <button
+                    type="submit"
+                    class="px-4 py-2 mt-2 w-18 text-center rounded-md bg-teal-500 shadow-md text-white hover:bg-teal-600 hover:shadow-lg font-semibold transition">create</button>
+                <button
+                    type="button"
+                    class="px-4 py-2 mt-2 w-18 text-center rounded-md bg-gray-400 shadow-md text-white hover:bg-gray-500 hover:shadow-lg font-semibold transition"
+                    @click="$emit('close')"
+                >cancel</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,31 +1,35 @@
 <template>
-    <div class="flex justify-center ">
-        <aside class="w-1/5 mr-2 bg-white/70 flex flex-col">
-            <h1 class="text-3xl font-extrabold text-center text-teal-700 mt-4 mb-6 tracking-wide">Home</h1>
+        <div class="flex justify-center ">
+            <aside class="w-1/5 mr-2 bg-white/70 flex flex-col">
+                <h1 class="text-3xl font-extrabold text-center text-teal-700 mt-4 mb-6 tracking-wide">Home</h1>
 
-            <UserBox class="mb-2"/>
+                <UserBox class="mb-2"/>
 
-            <button
-            class="w-full bg-teal-500 hover:bg-teal-600 text-white font-bold py-2 px-4 rounded-lg shadow-md transition"
-            >Create Post</button>
-        </aside>
+                <button
+                class="w-full bg-teal-500 hover:bg-teal-600 text-white font-bold py-2 px-4 rounded-lg shadow-md transition"
+                @click="showModal=true"
+                >Create Diary</button>
+            </aside>
 
-        <div class="mainBox w-1/3 gap-1 flex overflow-scroll flex-col">
-            <div class="flex p-1 border-2 border-teal-300 gap-1 sticky top-0 backdrop-blur-sm z-10 bg-teal-100/80 shadow-md rounded-md">
-                <button class="flex-1 font-bold hover:underline hover:text-gray-700 transition">Timeline</button>
-                <button class="flex-1 font-bold border-l-2 border-teal-400 hover:underline hover:text-gray-700 transition">Like</button>
+            <div class="mainBox w-1/3 gap-1 flex overflow-scroll flex-col">
+                <div class="flex p-1 border-2 border-teal-300 gap-1 sticky top-0 backdrop-blur-sm z-10 bg-teal-100/80 shadow-md rounded-md">
+                    <button class="flex-1 font-bold hover:underline hover:text-gray-700 transition">Timeline</button>
+                    <button class="flex-1 font-bold border-l-2 border-teal-400 hover:underline hover:text-gray-700 transition">Like</button>
+                </div>
+                <PostCard 
+                    v-for="(post, index) in posts"
+                    :key="index"
+                    :post="post"
+                />
             </div>
-            <PostCard 
-                v-for="(post, index) in posts"
-                :key="index"
-                :post="post"
-            />
         </div>
-    </div>
+        <PostModal v-if="showModal" @close="showModal=false"/>
 </template>
 
 <script setup lang="ts">
 import type { Post } from '~/types/type';
+
+const showModal = ref<boolean>(false)
 
 const posts: Post[] = [
      {
