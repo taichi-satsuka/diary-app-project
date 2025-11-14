1ddfc36de586b537fdb8a853b0a0ec112a472dfd
Taichi Satsuka <taichisatsuka@PC22027.local>
2025-11-14 15:53:54 +0900

    feat: link to / in default header

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="h-screen flex flex-col bg-gradient-to-br from-teal-50 to-teal-200">
         <header class="h-20 bg-teal-400 text-white flex items-center justify-center shadow-md ">
-            <h2 class="text-5xl">Diary App</h2>
+            <NuxtLink to="/" class="text-5xl">Diary App</NuxtLink>
         </header>
         <div class="flex-1">
             <slot />
