bc947415433034fb42c6baaa8479b46514d8bf4f
Taichi Satsuka <taichisatsuka@PC22027.local>
2025-11-14 16:47:45 +0900

    delete: bg-color

--- frontend/components/PostCard.vue
+++ frontend/components/PostCard.vue
@@ -2,7 +2,7 @@
     <div>
         <NuxtLink v-if="postSummary" :to="`/posts/${postSummary.id}`" class="w-full bg-white border border-gray-300 hover:bg-gray-100 rounded-2xl flex shadow-md py-2 pr-2 mb-1">
             <div class="flex-shrink-0 pl-2">
-                <svg data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" class="w-8 h-8  rounded-full">
+                <svg data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" class="w-8 h-8 rounded-full">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"></path>
                 </svg>
             </div>
