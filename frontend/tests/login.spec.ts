8113caad4beecbe96db6d24c0b41d4651df5d67c
Taichi Satsuka <taichisatsuka@PC22027.local>
2025-12-12 17:49:44 +0900

    modify

--- frontend/tests/login.spec.ts
+++ frontend/tests/login.spec.ts
@@ -1,34 +1,14 @@
 import { test, expect } from '@playwright/test';
+import { login, logout } from './utils/auth'
 
 test("login", async ({ browser}) => {
     const context = await browser.newContext()
     const page = await context.newPage()
-    const userName = "tanaka"
+    const userName = "yamanaka"
+    const password = "secret444"
 
-    await page.goto(
-        'http://diary-app-lb-1869033578.ap-northeast-1.elb.amazonaws.com/login',
-        {
-            waitUntil: 'domcontentloaded',
-            timeout: 60000
-        }
-    );
+    await login(page, userName, password)
+    await logout(page)
 
-    // 入力フォーム
-    const emailInput = page.getByRole('textbox', { name: /your email/i });
-    await expect(emailInput).toBeVisible();
-    await emailInput.fill(`${userName}@example.com`);
-
-    const passwordInput = page.getByRole('textbox', { name: 'password', exact: true });
-    await expect(passwordInput).toBeVisible();
-    await passwordInput.fill('secret444');
-
-    // ログインボタン
-    const loginButton = page.getByRole('button', { name: /Sign in/i });
-    await expect(loginButton).toBeVisible();
-    await loginButton.click();
-
-    // ホームページに遷移
-    await page.waitForURL("**", { timeout: 60000 });
-
-    context.close()
+    await context.close()
 });
--- frontend/tests/signup.spec.ts
+++ frontend/tests/signup.spec.ts
@@ -1,43 +1,40 @@
 import { test, expect } from '@playwright/test';
+import { waitForAttribute, waitForVisible } from './utils/waiter';
 
 test("sign up ", async ({ browser }) => {
     const context = await browser.newContext()
     const page = await context.newPage()
-    const userName = "hana"
+    const userName = "akasaka"
+    const url = "http://localhost/login";
 
-    await page.goto(
-        'http://diary-app-lb-1869033578.ap-northeast-1.elb.amazonaws.com/login',
-        {
-            timeout: 120000
-        }
-    );
+    await page.goto(url);
 
     // Sign up に遷移
     const signupLink = page.getByRole('link', { name: /Sign up/i });
-    await expect(signupLink).toBeVisible();
+    waitForVisible(signupLink)
     await signupLink.click();
     await page.waitForURL("**/register", { timeout: 30000 });
 
     // 入力フォーム
     const nameInput = page.getByRole('textbox', { name: /name/i });
-    await expect(nameInput).toBeVisible();
+    waitForVisible(nameInput);
     await nameInput.fill(userName);
 
     const emailInput = page.getByRole('textbox', { name: /your email/i });
-    await expect(emailInput).toBeVisible();
+    waitForVisible(emailInput);
     await emailInput.fill(`${userName}@example.com`);
 
     const passwordInput = page.getByRole('textbox', { name: 'password', exact: true });
-    await expect(passwordInput).toBeVisible();
+    waitForVisible(passwordInput);
     await passwordInput.fill('secret444');
 
     const passwordConfirmInput = page.getByRole('textbox', { name: 'password confirmation', exact: true });
-    await expect(passwordConfirmInput).toBeVisible();
+    waitForVisible(passwordConfirmInput);
     await passwordConfirmInput.fill('secret444');
 
     // 作成ボタン
     const createButton = page.getByRole('button', { name: /create/i });
-    await expect(createButton).toBeVisible();
+    waitForVisible(createButton);
     await createButton.click();
 
     // ログインページに遷移
@@ -51,25 +48,21 @@ test("sign up with existing email show error", async ({ browser}) => {
     const context = await browser.newContext()
     const page = await context.newPage()
     const userName = "akiba"
+    const url = "http://localhost/register";
 
-    await page.goto(
-        'http://diary-app-lb-1869033578.ap-northeast-1.elb.amazonaws.com/login',
-        {
-            timeout: 120000
-        }
-    );
+    await page.goto(url);
 
     // 入力フォーム
     const nameInput = page.getByRole('textbox', { name: /name/i });
-    await expect(nameInput).toBeVisible();
+    waitForVisible(nameInput);
     await nameInput.fill(userName);
 
     const emailInput = page.getByRole('textbox', { name: /your email/i });
-    await expect(emailInput).toBeVisible();
+    waitForVisible(emailInput);
     await emailInput.fill(`${userName}@example.com`);
 
     const passwordInput = page.getByRole('textbox', { name: 'password', exact: true });
-    await expect(passwordInput).toBeVisible();
+    waitForVisible(passwordInput);
     await passwordInput.fill('secret444');
 
     const passwordConfirmInput = page.getByRole('textbox', { name: 'password confirmation', exact: true });
@@ -78,12 +71,12 @@ test("sign up with existing email show error", async ({ browser}) => {
 
     // 作成ボタン
     const createButton = page.getByRole('button', { name: /create/i });
-    await expect(createButton).toBeVisible();
+    waitForVisible(createButton);
     await createButton.click();
 
     // エラーメッセージを検証
     const errorMessage = page.locator('p', { hasText: /email.*already.*taken/i });
-    await expect(errorMessage).toBeVisible();
+    waitForVisible(errorMessage);
     await expect(errorMessage).toHaveText(/The Email has already been taken/i);
 
     // URL は登録できないので /register のまま
